48c80efca6a672556d56bfeb6b728fb823d7205c
ahmedalawar <af47037@gmail.com>
2023-09-07 19:31:08 -0700

    3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int puts(const char* str);
-int main ()
-{char mystr[] = "\"Programming is like building a multilingual puzzle";
+int main (void)
+{
+	char mystr[] =("\"Programming is like building a multilingual puzzle");
 
 	puts(mystr);
 
-	return 0;}	
+	return (0);}	
