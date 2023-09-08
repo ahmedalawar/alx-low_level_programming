cadefe7a89d324eda893a35ca916edd495be9087
ahmedalawar <af47037@gmail.com>
2023-09-07 23:49:16 -0700

    size_of_variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char:%lu byte(s)", sizeof(char));
-printf("\nSize of a int:%lu byte(s)", sizeof(int));
-printf("\nSize of a long int:%lu byte(s)", sizeof(long int));
-printf("\nSize of a int:%lu byte(s)", sizeof(long long));
-printf("\nSize of a float:%lu byte(s)", sizeof(float));
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("\nSize of an int: %lu byte(s)", sizeof(int));
+printf("\nSize of a long int: %lu byte(s)", sizeof(long int));
+printf("\nSize of a long long :%lu byte(s)", sizeof(long long));
+printf("\nSize of a float: %lu byte(s)", sizeof(float));
 return (0);
 }
