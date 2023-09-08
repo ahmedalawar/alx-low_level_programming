3383a052dfe6b3ee0bf06d3b405d351c2df14f91
ahmedalawar <af47037@gmail.com>
2023-09-07 19:48:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
-int puts(const char* str);
-int main (void)
+int puts(const char *str);
+int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return (0);
 
-
-	return (0);}	
+	}	
