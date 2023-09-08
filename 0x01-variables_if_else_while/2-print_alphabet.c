966b2111ba6eea6e04422f62eb574142cbef2acc
ahmedalawar <af47037@gmail.com>
2023-09-08 16:28:28 -0700

    printing alphabit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
 *main - this function print all lower case letters
+*	using assci code and while loop.
+*
 *Return = always (0)
 */
 int main(void)
@@ -9,7 +11,7 @@ int c = 97;
 
 while (c <= 122)
 {
-	putchar(c);
+	putchar(c"\n");
 	c++;
 	}
 	return (0);
