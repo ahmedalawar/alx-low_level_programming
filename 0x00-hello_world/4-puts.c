2cb27584b6def3fda66e5fed75272035746c6004
ahmedalawar <af47037@gmail.com>
2023-09-07 19:38:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
 int puts(const char* str);
 int main (void)
 {
-	char mystr[] ="\"Programming is like building a multilingual puzzle";
+	puts( "\"Programming is like building a multilingual puzzle" );
 
-	puts(mystr);
+
 
 	return (0);}	
