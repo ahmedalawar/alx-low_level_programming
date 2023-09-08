4b2ace666ffbf237aaaa4cf6bb56af57bc09d579
ahmedalawar <af47037@gmail.com>
2023-09-08 16:20:03 -0700

    ascci_code_to_print_loweralphabit_letters 97 to 122

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - this function print all lower case letters
+*Return = always (0)
+*/
+int main(void)
+{
+for (int c = 97; c <= 122; c++)
+{
+	putchar(c);
+	}
+	return (0);
+}
