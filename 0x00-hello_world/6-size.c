0a21ace4df69fa49b926b076898c3ddc5deeb94a
ahmedalawar <af47037@gmail.com>
2023-09-07 20:16:18 -0700

    sizeof_variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *size of variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:", sizeof(char));
+printf("\nSize of a int:", sizeof(int));
+printf("\nSize of a long int:", sizeof(long int));
+printf("\nSize of a float:", sizeof(float));
+return (0);
+}
