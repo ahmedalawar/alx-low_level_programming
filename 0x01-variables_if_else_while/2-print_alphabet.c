9d33e743f87787ea0bc403a408edb55c53c70d0b
ahmedalawar <af47037@gmail.com>
2023-09-08 19:39:37 -0700

    printing alphabit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
+
 /**
-*main - this function print all lower case letters
-*	using assci code and while loop.
-*
-*Return = always (0)
-*/
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-int c = 97;
+	char letter;
 
-while (c <= 122)
-{
-	putchar(c);
-	c++;
-	}
-	putchar(' ')
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
 	return (0);
-
 }
