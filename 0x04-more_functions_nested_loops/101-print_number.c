0686a5724b1db53145f19065ba5d059d25ceeabd
ahmedalawar <af47037@gmail.com>
2023-09-15 20:06:12 -0700

    adva

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 
 	if (n < 10 && n >= 0)
 	{
-		_putchar (n + '0');
+		putchar (n + '0');
 	}
 	else if (n > -10 && n < 0)
 	{
 		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
+		putchar('-');
+		putchar (n + '0');
 	}
 
 	else
@@ -28,7 +28,7 @@ void print_number(int n)
 		if (n < 0)
 		{
 			n = n * -1;
-			_putchar ('-');
+			putchar ('-');
 		}
 		z = n;
 	while (z / b > 9)
@@ -39,7 +39,7 @@ void print_number(int n)
 	{
 		m = z / b;
 		z = z % b;
-		_putchar (m + '0');
+		putchar (m + '0');
 		b = b / 10;
 	}
 	}
