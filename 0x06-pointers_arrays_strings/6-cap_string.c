9ab0dd2c7544bdd9349ef5653301ed2125803f80
ahmedalawar <142682488+ahmedalawar@users.noreply.github.com>
2023-09-21 19:37:39 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,86 +11,86 @@ char *cap_string(char *a)
 
 	while (a[i] != '\0')
 	{
-	switch (a[i])
+switch (a[i])
+{
+case ' ':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
 	{
-	case ' ':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
+	a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '\t':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
 		a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '\t':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '\n':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case ',':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case ';':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '.':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '!':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '?':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '"':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '(':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case ')':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '{':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
-	case '}':
-		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
-		{
-			a[i + 1] = toupper(a[i + 1]);
-		}
-		break;
+	}
+	break;
+case '\n':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case ',':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case ';':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '.':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '!':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '?':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '"':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '(':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case ')':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '{':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
+case '}':
+	if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+	{
+		a[i + 1] = toupper(a[i + 1]);
+	}
+	break;
 		}
 		i++;
 		}
