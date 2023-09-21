edff8f15b85d077fece2923fb1ee15c41c25070a
ahmedalawar <af47037@gmail.com>
2023-09-21 00:02:08 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_strcmp - function that compares two strings.
+*@s1: pointer that indicate the first comparator
+*@s2: pointer that indicates the second comprator
+*Return:(0) if two strings are identcal
+*	(-) If the first character that doesn't match has a lower value in the
+*		first string compared to the second
+*	(+) the first non-matching character has a higher value in the first string
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	while (s1[j] != '\0' || s2[j] != '\0')
+	{
+		if (s1[j] == s2[j])
+		{
+			i += 0;
+		}
+		else if (s1[j] < s2[j])
+		{
+			i -= 1;
+		}
+		else if (s1[j] > s2[j])
+		{
+			i += 1;
+		}
+	}
+	return (i);
+}
