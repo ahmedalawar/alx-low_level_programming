3ba7135857ef1943f14c897ea658799ec97ebf8b
ahmedalawar <af47037@gmail.com>
2023-09-21 01:36:50 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,12 @@ void reverse_array(int *a, int n)
 {
 	int i, y, z;
 
-	z = n;
+	z = n - 1;
 	for (i = 0; i < n / 2 ; i++)
-		{
-			y = a[i];
-			a[i] = a[z];
-			a[z] = y;
-			z--;
-		}
+	{
+		y = a[i];
+		a[i] = a[z];
+		a[z] = y;
+		z--;
 	}
 }
