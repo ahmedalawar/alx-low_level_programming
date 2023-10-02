840f30245c62601f3b93e61876e28c32d65a1430
ahmedalawar <af47037@gmail.com>
2023-10-01 18:34:42 -0700

    0x08. C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,41 @@
 #include "main.h"
 /**
-*is_palindrome - Write a function that returns 1 
+*is_palindrome_helper - Write a function that returns 1
 *		if a string is a palindrome and 0 if not.
 *@s: string
+*@start: start point.
+*@end: end point of string.
 *Return: 1 if palindrome 0 if not
 */
 
-int is_palindrome(char *s)
+int is_palindrome_helper(char *s, int start, int end)
 {
-	int i =  0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	if (i <= 1)
+	if (start <= end)
 	{
 		return (1);
 	}
-	else if (s[0] != s[i - 1])
+	else if (s[start] != s[end])
 	{
 		return (0);
 	}
 	else
 	{
-		return (is_palindrome(s++));
+		return (is_palindrome_helper(s, start + 1, end - 1));
 	}
+}
+
+/**
+*is_palindrome - write a function to check palindrome
+*@s: string
+*Return: 1 if palindrome 0 otherwise.
+*/
+int is_palindrome(char *s)
+{
+	int len =  0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (is_palindrome_helper(s, 0, len - 1));
 }
