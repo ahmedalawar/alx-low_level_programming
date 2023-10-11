d4de90b8a7c0f1886dcefd8a9d35045dd93374b0
ahmedalawar <af47037@gmail.com>
2023-10-10 23:31:02 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s\n",d->name);
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age == 0)
 	{
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Age: %f\n",d->age);
+		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
@@ -29,6 +29,6 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Owner: %s\n",d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
