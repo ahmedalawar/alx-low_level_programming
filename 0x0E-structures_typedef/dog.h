666e4848f9293f02bfe9c7fbcdcb16221004610e
ahmedalawar <af47037@gmail.com>
2023-10-10 22:18:29 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+*struct dog - dog specifications
+*@name: first argument take dog name
+*@age: second arg take dog age
+*@owner: third argument take owner
+*
+*Describtion: long for dogs
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
