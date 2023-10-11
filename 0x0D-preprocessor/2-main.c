e5efeb39c5ffe7800129eb85364b9efaa143e812
ahmedalawar <af47037@gmail.com>
2023-10-10 19:30:47 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - funcation that print file name followed by a new line.
+*Return: Always (0).
+*/
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return 0;
+}
