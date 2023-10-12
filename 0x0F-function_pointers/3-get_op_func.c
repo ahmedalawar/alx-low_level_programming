3d3addaa0bdd999bf92454d0ba49ab68c4945730
ahmedalawar <af47037@gmail.com>
2023-10-11 18:31:17 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,15 @@
 #include "3-calc.h"
 #include <stddef.h>
+
 /**
-*get_op_func - function that selects the correct function to perform the operation
+*get_op_func - function that selects the correct
+*		function to perform the operation
 *@s: operator passed as argument to the program
 *@a: integar
 *@b: integar
 *Return: function
 */
+
 int (*get_op_func(char *s))(int, int)
 {
 op_t op_s[] = {
@@ -18,6 +21,7 @@ op_t op_s[] = {
 		{NULL, NULL}
 	};
 	int i = 0;
+
 	while (op_s[i].op)
 	{
 		if (*(op_s[i].op) == *s)
