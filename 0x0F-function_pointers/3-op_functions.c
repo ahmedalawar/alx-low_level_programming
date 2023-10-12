28ab2097a0fc2c005f5cff62726d244bf3e99412
ahmedalawar <af47037@gmail.com>
2023-10-11 17:57:32 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+*op_add - sum function
+*@a: integar
+*@b: integar
+*Return: Sum of a + b
+*/
+int op_add(int a, int b)
+{
+	return ((a) + (b));
+}
+/**
+*op_sub - sub function
+*@a: integar
+*@b: integar
+*Return: Sub of a + b
+*/
+int op_sub(int a, int b)
+{
+	return ((a) - (b));
+}
+/**
+*op_mul - multplication function
+*@a: integar
+*@b: integar
+*Return: mul of a + b
+*/
+int op_mul(int a, int b)
+{
+	return ((a) * (b));
+}
+/**
+*op_div - sum function
+*@a: integar
+*@b: integar
+*Return: div of a + b
+*/
+int op_div(int a, int b)
+{
+	return ((a) / (b));
+}
+/**
+*op_mod - sum function
+*@a: integar
+*@b: integar
+*Return: mod of a + b
+*/
+int op_mod(int a, int b)
+{
+	return ((a) % (b));
+}
