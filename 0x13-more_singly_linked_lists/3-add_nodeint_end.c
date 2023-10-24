fdcdacdbc8e469d2c0afe6f84cc90b4f506c6add
ahmedalawar <af47037@gmail.com>
2023-10-24 13:40:11 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,11 +13,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	temp->n = n;
 	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
 	ptr = *head;
 	while (ptr)
 	{
 		ptr = ptr->next;
 	}
 	ptr->next = temp;
+	}
 	return (temp);
 }
