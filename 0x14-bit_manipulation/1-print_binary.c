651fa02fc8c7e43844febf2443c7cf32bf15095f
ahmedalawar <af47037@gmail.com>
2023-10-27 08:12:04 -0700

    printing_binary_number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*print_binary - prints the binary representation of a number.
+*@n: decimal num.
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+	unsigned long int i;
+	bool start_printing = false;
+
+	for (i = 0; i < sizeof(n) * 8; i++)
+	{
+		if (start_printing)
+		{
+			if (n & mask)
+				putchar('1');
+			else
+				putchar('0');
+		}
+		else if (n & mask)
+		{
+			start_printing = true;
+				putchar('1');
+		}
+		mask = mask >> 1;
+	}
+	if (start_printing == false)
+		putchar('0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
