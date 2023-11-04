c15f760cb277f25ed3faa497ed04b414cb26d78c
ahmedalawar <af47037@gmail.com>
2023-11-04 16:33:17 -0700

    0x15.C FILE

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,41 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
 /**
-*ssize_t read_textfile - reads a text file and prints it to the POSIX standard output.
-*@filename: file name
-*@letters: is the number of letters it should read and print
-*Return: the actual number of letters it could read and print
-*/
+ * read_textfile - that reads a text file and prints
+ * @filename: variable pointer
+ * @letters: size letters
+ * Description: Write a function that reads a text file and prints it
+ * to the POSIX standard output.
+ * Return: the actual number of letters it could read and print, 0 otherwise
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	FILE *file;
-	char *buffer;
-	ssize_t b_read, b_write;
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY);
 
-	if (fd == -1)
-		return (0);
-
-	file = fopen(filename, "r");
-
-	if (file == NULL)
-		return (0);
-
-	 buffer = malloc(letters + 1);
-
-	if (buffer == NULL)
+	if (file == -1)
 	{
-		fclose(file);
+		free(text);
 		return (0);
 	}
 
-	b_read = fread(buffer, 1, letters,file);
+	let = read(file, text, letters);
 
-	if (b_read == -1)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	fclose(file);
+	w = write(STDOUT_FILENO, text, let);
 
-	 b_write = write(STDOUT_FILENO, buffer, b_read);
+	close(file);
 
-	free(buffer);
-
-	if (b_write == -1 || b_write != b_read)
-		return (0);
-	return b_read;
+	return (w);
 }
