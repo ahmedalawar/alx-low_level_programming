ec9c5f526fbfa209fb20b1f29e4b2bd74102a051
ahmedalawar <af47037@gmail.com>
2023-11-04 12:01:18 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+*ssize_t read_textfile - reads a text file and prints it to the POSIX standard output.
+*@filename: file name
+*@letters: is the number of letters it should read and print
+*Return: the actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	int fd = open(filename, 0_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+	char *buffer = malloc(letters + 1);
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t b_read = fread(buffer, 1, letters,file);
+
+	if (b_read == -1)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+	fclose(file);
+
+	ssize_t b_write = write(STDOUT_FILENO, buffer, b_read);
+
+	free(buffer);
+
+	if (b_write == -1 || b_write != b_read)
+		return (0);
+	return b_read;
+}
