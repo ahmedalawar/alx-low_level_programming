e9eaa7e0cd5ab6607f0808c058540757a18c048d
ahmedalawar <af47037@gmail.com>
2023-11-04 17:10:15 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 *create_file - creates a file.
 *@filename:  is the name of the file to create
 *@text_content: text_content is a NULL terminated string to write to the file
-*Returns: 1 on success, -1 on failure
+*Return: 1 on success, -1 on failure
 */
 int create_file(const char *filename, char *text_content)
 {
@@ -19,12 +19,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		i++;
 	}
-	
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
-	
+
 	write(file, text_content, i);
 
 	return (1);
