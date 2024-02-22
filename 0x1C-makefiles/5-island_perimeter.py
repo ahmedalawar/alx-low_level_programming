c0b74b0de1dc0d096b99be99cc82ff6739ac5846
ahmed alawar <af47037@gmail.com>
2024-02-21 23:40:27 -0800

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
-"""
+    """
     Calculate the perimeter of an island in a 2D grid.
 
-    The function iterates over each cell in the grid. If the cell is land (represented by 1), 
-    it adds 4 to the perimeter (since a standalone square has 4 sides). Then, it checks the 
-    cell above and to the left. If either is also land, it subtracts 2 from the perimeter 
-    (since each shared side with a neighbor doesn't contribute to the perimeter).
+    The function iterates over each cell in the grid. If the cell is land
+    (represented by 1),
+    it adds 4 to the perimeter (since a standalone square has 4 sides). Then,
+    it checks the cell above and to the left. If either is also land,
+    it subtracts 2 from the perimeter
+    (each shared side with a neighbor doesn't contribute to the perimeter).
 
     Parameters:
-    grid (List[List[int]]): A 2D list representing the grid. Land is represented by 1, and water is represented by 0.
+    grid (List[List[int]]): A 2D list representing the grid.
+    Land is represented by 1, and water is represented by 0.
 
     Returns:
     int: The perimeter of the island in the grid.
